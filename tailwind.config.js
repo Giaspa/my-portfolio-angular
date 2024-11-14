787ecc2d44aeeb8ceff52a9ddea237c597733a24
Gianluca Spadazzi <spadazzi.gianluca@gmail.com>
2024-11-14 20:52:20 +0100

    First layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'futuristic': '0 10px 20px rgba(0, 0, 0, 0.3), 0 6px 6px rgba(0, 0, 0, 0.2)',
+      },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(121,9,9,1) 0%, rgba(0,0,0,1) 100%)'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
