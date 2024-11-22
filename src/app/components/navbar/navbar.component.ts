e101b9ad88e299be7aa88a5cc330e8f91ff821df
Gianluca Spadazzi <spadazzi.gianluca@gmail.com>
2024-11-22 11:46:33 +0100

    Edit responsivity

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { FrameworkIconComponent } from "./framework-icon/framework-icon.component";
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FrameworkIconComponent, RouterLink, RouterLinkActive],
+  imports: [FrameworkIconComponent, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+   isDropdownOpen = false;
 
+  closeDropdown(): void {
+    this.isDropdownOpen = false;
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
